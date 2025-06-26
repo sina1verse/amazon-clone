c69bbff67edd9c2a58b73a30e38678f9a879d3ce
sina <cyrus2nd.tg@gmail.com>
2025-06-26 14:11:02 -0400

    Syncronize cart quantity with total quantity in amazon.html and checkout.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,8 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-
+updateCartQuantity();
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -60,15 +60,10 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
 function updateCartQuantity() {
-  let cartQuantity = 0;
-
-      cart.forEach((cartItem) => {
-        cartQuantity += cartItem.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = calculateCartQuantity();
 }
 
 document.querySelectorAll('.js-add-to-cart')
@@ -78,6 +73,6 @@ document.querySelectorAll('.js-add-to-cart')
       
       addToCart(productId);
       updateCartQuantity();
-      
     });
   });
+
