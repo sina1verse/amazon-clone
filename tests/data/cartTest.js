694894bc699defb954ec955883aeca55b5b495fb
sina <cyrus2nd.tg@gmail.com>
2025-07-03 03:16:07 -0400

    Add removeFromCart test suite to cartTest.js

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
 
   
 describe('test suite: addToCart', () => {
@@ -53,3 +53,61 @@ describe('test suite: addToCart', () => {
     }]));
   });
 });
+
+describe('test suite: removeFromCart', () => {
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]);
+    });
+
+    loadFromStorage();
+  });
+  
+  it('removes a productId that is in the cart', () => {
+    removeFromCart(productId1);
+    
+    const found = cart.some(cartIem => 
+      cartIem.productId === productId1 
+    );
+    expect(found).toBeFalse();
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }])
+    );
+  });
+  
+  
+  it('does nothing if product is not in the cart', () => {
+    removeFromCart('this-is-a-none-existent-id');
+    
+    const found =
+      cart[0].productId === productId1 && cart[1].productId === productId2;  
+    
+    expect(found).toBeTrue();
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: productId1,
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: productId2,
+      quantity: 1,
+      deliveryOptionId: '2'
+    }]));
+  });
+})
