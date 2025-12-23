0c74273224812a431df970fd25935f0ae7fd3e5b
Sina <cyrus2nd.tg@gmail.com>
2025-12-23 05:35:40 -0500

    Improve documentation

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,35 +1,42 @@
+/*
+  Description:
+  JavaSript file To implement tests
+  These are simple tests made in vanila JavaScript
+  No frameworks used
+*/
+
 import { formatCurrency } from "../../scripts/utils/money.js";
 
-console.log('test suite: formatCurrency');
+console.log("test suite: formatCurrency");
 
-console.log('converts cents into dollars');
+console.log("converts cents into dollars");
 
-if (formatCurrency(2095) === '20.95') {
-  console.log('passed');
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
 } else {
-  console.log('failed');
+  console.log("failed");
 }
 
-console.log('works with 0');
+console.log("works with 0");
 
-if (formatCurrency(0) === '0.00') {
-  console.log('passed');
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
 } else {
-  console.log('failed');
+  console.log("failed");
 }
 
-console.log('rounds up to the nearest cent');
+console.log("rounds up to the nearest cent");
 
-if (formatCurrency(2000.5) === '20.01') {
-  console.log('passed');
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
 } else {
-  console.log('failed');
+  console.log("failed");
 }
 
-console.log('rounds down to the nearest cent');
+console.log("rounds down to the nearest cent");
 
-if (formatCurrency(2000.4) === '20.00') {
-  console.log('passed');
+if (formatCurrency(2000.4) === "20.00") {
+  console.log("passed");
 } else {
-  console.log('failed');
+  console.log("failed");
 }
